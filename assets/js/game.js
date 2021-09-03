ae7a10d72b6916e992d3c19acf77707d863d9d78
insomniKat <ru3ykat@gmail.com>
2021-09-03 17:51:54 -0400

    random health and attack values

--- assets/js/game.js
+++ assets/js/game.js
@@ -4,13 +4,22 @@
 //      * Defeat each enemy-robot
 // "LOSE" - Player robot's health is zero or less
 
+
+
+// function to generate a random numeric value
+var randomNumber = function(min, max) {
+    var value = Math.floor(Math.random() * (max -min + 1) + min);
+
+    return value;
+};
+
 var playerName = window.prompt("What is your robot's name?");
 var playerHealth = 100;
 var playerAttack = 10;
 var playerMoney = 10;
 
 var enemyNames = ["Roborto", "Amy Android", "Robo Trumble"];
-var enemyHealth = 50;
+var enemyHealth = randomNumber(40, 60);
 var enemyAttack = 12;
 
 // fight function (now with parameter for enemy's name)
@@ -29,14 +38,17 @@ var fight = function(enemyName) {
             if (confirmSkip) {
                 window.alert(playerName + ' has decided to skip this fight. Goodbye!');
                 // subtract money from playerMoney for skipping
-                playerMoney = playerMoney - 10;
+                playerMoney = (Math.max(0, playerMoney - 10));
                 console.log("playerMoney", playerMoney)
                 break;
             }
         }
 
-        // remove enemy's health by subtracting the amount set in the playerAttack variable
-        enemyHealth = enemyHealth - playerAttack;
+        // generate random damage value based on player's attack power
+        var damage = randomNumber(playerAttack - 3, playerAttack);
+
+        // remove enemy's health by subtracting the amount calculated in damage
+        enemyHealth = (Math.max(0, enemyHealth - damage));
         console.log(
             playerName + ' attacked ' + enemyName + '. ' + enemyName + ' now has ' + enemyHealth + ' health remaining.'
         );
@@ -54,8 +66,11 @@ var fight = function(enemyName) {
               window.alert(enemyName + ' still has ' + enemyHealth + ' health left.');
           }
 
-          // remove player's health by subtracting the amount set in the enemyAttack variable
-          playerHealth = playerHealth - enemyAttack; 
+          // generate random damage value based on enemy's attack power
+          var damage = randomNumber(enemyAttack -3, enemyAttack);
+          
+          // remove player's health by subtracting the amount set in damage
+          playerHealth = (Math.max(0, playerHealth - damage)); 
           console.log(
               enemyName + ' attacked ' + playerName + '. ' + playerName + ' now has ' + playerHealth + ' health remaining.'
           );
